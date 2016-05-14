89e27f60be8047f6508990541f39368e19312ab8
Hasan Arous <arous.hasan@gmail.com>
2016-05-14 13:15:19 +0300

    Add language field

--- models/Book.js
+++ models/Book.js
@@ -4,6 +4,16 @@ var mongoose  = require('mongoose')
 var schema = mongoose.Schema({
   name: String,
   altName: String,
+  language: {type: String, $p: {
+    widget: 'select',
+    formFieldOpts: {
+      choices: {
+        ar: 'Arabic',
+        en: 'English',
+        'ar,en': 'Arabic/English',
+        fr: 'French',
+      },
+  }}},
   author: {type: mongoose.Schema.Types.ObjectId, ref: 'Person', $p: {widget: 'select'}},
   category: {type: mongoose.Schema.Types.ObjectId, ref: 'Category', $p: {widget: 'select'}},
   publisher: {type: mongoose.Schema.Types.ObjectId, ref: 'Publisher', $p: {widget: 'select'}},
